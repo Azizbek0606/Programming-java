384d4c8d3dab7babfa7c7af6a387e8d784f2f4f3
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-01-16 17:34:06 +0500

    create

---
+++ final_exam_pdp_programming/Azizbek_xabibullayev_programming/Main.java
@@ -0,0 +1,192 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main
+{
+    static ArrayList<String> participantArr = new ArrayList<>();
+    static ArrayList<ArrayList<Integer>> participantBallArr = new ArrayList<>();
+    static ArrayList<String> participantTeamMembers = new ArrayList<>();
+
+    static int getPositiveInput(String innerText) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.print(innerText);
+                int userInput = Integer.parseInt(scanner.nextLine());
+                if (userInput >= 2) {
+                    return userInput;
+                } else {
+                    System.out.println("Noto'g'ri qiymat. Kamida 2 yoki undan yuqori son kiriting.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Noto'g'ri qiymat. Faqat son kiriting.");
+            }
+        }
+    }
+
+    static String getParticipantName(int index, String alertInput) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print((index + 1) + "-" + alertInput + ":\n");
+            String participant = scanner.nextLine();
+            if (!participant.isEmpty()) {
+                return participant;
+            }
+        }
+    }
+
+    static int getBallPlayers(int participantIndex, String inputText) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.print("\n" + (participantIndex + 1) + " - " + inputText + ": " +
+                        participantArr.get(participantIndex) + " uchun ball: ");
+                int ball = Integer.parseInt(scanner.nextLine());
+                if (ball >= 0) {
+                    return ball;
+                } else {
+                    System.out.println("Noto'g'ri qiymat. Faqat son kiriting.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Noto'g'ri qiymat. Faqat son kiriting.");
+            }
+        }
+    }
+
+    static void alone() {
+        int participantCount = getPositiveInput("\nIshtirokchi sonini kiriting: ");
+        System.out.println("-----------------------------------------------------------------------");
+        for (int i = 0; i < participantCount; i++) {
+            participantArr.add(getParticipantName(i, "ishtirokchi ismini kiriting"));
+        }
+        System.out.println("-----------------------------------------------------------------------");
+
+        System.out.println("\nO'yin boshlandi.\nJami ishtirokchilar soni " + participantCount + "ta ular:\n");
+
+        for (int i = 0; i < participantArr.size(); i++) {
+            System.out.println((i + 1) + " - ishtirokchi: " + participantArr.get(i));
+        }
+        System.out.println("-----------------------------------------------------------------------");
+        int gameCounter = 0;
+        for (int i = 0; i < 5; i++) {
+            System.out.println(
+                    "\n" + (gameCounter + 1) + " - bosqich yakunlandi\nishtirokchilarga " + (gameCounter + 1)
+                            + " - bosqich bo'yicha ball bering\n");
+            ArrayList<Integer> limitedArrForBall = new ArrayList<>();
+
+            for (int k = 0; k < participantArr.size(); k++) {
+                limitedArrForBall.add(getBallPlayers(k, "Ishtirokchi"));
+            }
+            gameCounter++;
+            participantBallArr.add(limitedArrForBall);
+        }
+        System.out.println("-----------------------------------------------------------------------");
+
+        int levelCounter = 0;
+        System.out.println("\nBarcha bosqichlar yakunlandi\nishtirokchilarning umumiy ballari:\n");
+        ArrayList<Integer> getMaxBall = new ArrayList<>();
+        for (int i = 0; i < participantArr.size(); i++) {
+            ArrayList<Integer> ballArr = new ArrayList<>();
+            for (ArrayList<Integer> k : participantBallArr) {
+                levelCounter++;
+                System.out.println(
+                        participantArr.get(i) + " " + levelCounter + " - bosqichda " + k.get(i) + ": ball oldi");
+                ballArr.add(k.get(i));
+                if (levelCounter == 5) {
+                    levelCounter = 0;
+                    System.out.println("\n\n");
+                    getMaxBall.add(ballArr.stream().mapToInt(Integer::intValue).sum());
+                }
+            }
+        }
+        int maxBall = getMaxBall.stream().mapToInt(Integer::intValue).max().orElse(0);
+        int maxBallIndex = getMaxBall.indexOf(maxBall);
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println("G'olib: " + participantArr.get(maxBallIndex) + "\njami to'plagan bali: " + maxBall);
+    }
+
+    static void team() {
+        int groupCount = getPositiveInput("Gurux sonini kiriting:\n");
+        int groupMember = getPositiveInput("Gurux a'zolari sonini kiriting:\n");
+        System.out.println("-----------------------------------------------------------------------");
+        for (int i = 0; i < groupCount; i++) {
+            participantArr.add(getParticipantName(i, "gurux nomini kiriting"));
+        }
+        for (int i = 0; i < groupMember * groupCount; i++) {
+            participantTeamMembers.add(getParticipantName(i, "ishtirokchining ismi"));
+        }
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println("\nO'yin boshlandi.\nJami ishtirokchilar soni " + groupMember * groupCount
+                + "ta ular:\n");
+
+        for (int i = 0; i < groupMember * groupMember; i++) {
+            System.out.println((i + 1) + " - ishtirokchi: " + participantTeamMembers.get(i));
+        }
+        System.out.println("-----------------------------------------------------------------------");
+        int gameCounter = 0;
+        for (int i = 0; i < 5; i++) {
+            System.out.println(
+                    "\n" + (gameCounter + 1) + " - bosqich yakunlandi\nguruxlarga " + (gameCounter + 1)
+                            + " - bosqich bo'yicha ball bering\n");
+            ArrayList<Integer> limitedArrForBall = new ArrayList<>();
+
+            for (int k = 0; k < participantArr.size(); k++) {
+                limitedArrForBall.add(getBallPlayers(k, "gurux"));
+            }
+            gameCounter++;
+            participantBallArr.add(limitedArrForBall);
+        }
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println("\nBarcha bosqichlar yakunlandi\nguruxlarning umumiy ballari:\n");
+        int levelCounter = 0;
+        ArrayList<Integer> getMaxBall = new ArrayList<>();
+        for (int i = 0; i < participantArr.size(); i++) {
+            ArrayList<Integer> ballArr = new ArrayList<>();
+            for (ArrayList<Integer> k : participantBallArr) {
+                levelCounter++;
+                System.out.println(
+                        participantArr.get(i) + " " + levelCounter + " - bosqichda " + k.get(i) + ": ball oldi");
+                ballArr.add(k.get(i));
+                if (levelCounter == 5) {
+                    levelCounter = 0;
+                    System.out.println("\n\n");
+                    getMaxBall.add(ballArr.stream().mapToInt(Integer::intValue).sum());
+                }
+            }
+        }
+        System.out.println("-----------------------------------------------------------------------");
+        int maxBall = getMaxBall.stream().mapToInt(Integer::intValue).max().orElse(0);
+        int maxBallIndex = getMaxBall.indexOf(maxBall);
+        System.out.println(
+                "G'olib: " + participantArr.get(maxBallIndex) + " guruhi\n. Jami to'plagan ballari: " + maxBall
+                        + "\n\n");
+    }
+
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println(
+                    "__________________________________________________________________________\n");
+            System.out.println("1 - Yakka tartib");
+            System.out.println("2 - Jamoavi tartib");
+            System.out.println("3 - Dasturni to'xtatish");
+            System.out.println(
+                    "\n__________________________________________________________________________");
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("\nFaqat son kiriting: ");
+            try {
+                int inputIndicator = Integer.parseInt(scanner.nextLine());
+                if (inputIndicator > 0 && inputIndicator <= 3) {
+                    if (inputIndicator == 1) {
+                        alone();
+                    } else if (inputIndicator == 2) {
+                        team();
+                    } else if (inputIndicator == 3) {
+                        break;
+                    }
+                }
+            } catch (NumberFormatException e) {
+                continue;
+            }
+        }
+    }
+}
